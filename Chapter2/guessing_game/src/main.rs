c6a902b3b057e1fcbe2e0cd3d65b1f1b80357316
CFdefense <152815652+CFdefense@users.noreply.github.com>
2025-02-12 23:15:29 -0500

    Chapters 1 and 2

---
+++ Chapter2/guessing_game/src/main.rs
@@ -0,0 +1,34 @@
+use std::{cmp::Ordering, io};
+use rand::Rng;
+
+fn main() {
+    println!("Guess The Number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please Input Your Guess");
+
+        let mut guess:String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => print!("Too Small"),
+            Ordering::Greater => print!("Too Large"),
+            Ordering::Equal => {
+                print!("You Win!");
+                break
+            },
+        }
+    }
+}
